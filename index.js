d08358cc168767575b88636b1dc4175df6e5845f
Maxwell Rogers <100175240+wiresgithub@users.noreply.github.com>
2022-05-21 12:09:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,32 @@ function newImage(url, left, bottom){
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
 }
+let inventory = newInventory()
 
-function newInventory(){
+newInventory()
+newImage('assets/green-character.gif', 100, 250)
+newImage('assets/tree.png', 200, 450)
+newImage('assets/pillar.png', 350, 250)
+newImage('assets/pine-tree.png', 450, 350)
+newImage('assets/crate.png', 150, 350)
+newImage('assets/well.png', 500, 575)
+
+newItem('assets/sword.png', 500, 555)
+newItem('assets/shield.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
+
+
+function newItem(url, left, bottom) {
+    let item = newImage(url, left, bottom)
+    item.addEventListener('click', function () {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
+}
+
+function newInventory() {
     let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
@@ -26,16 +50,5 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
-
-newInventory()
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
